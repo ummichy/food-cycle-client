6fa7504296e47b664becab71739348fbde8ec135
rowshonummi <rowshonummichowdhury@gmail.com>
2025-07-20 03:19:03 +0600

    Transtack added

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,5 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import './index.css';
-// import router from './Routes/Routes.jsx';
-// import { RouterProvider } from 'react-router';
-// import AuthProvider from './Provider/AuthProvider.jsx';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <AuthProvider>
-//       <RouterProvider
-//         router={router}
-//         fallbackElement={<div className="text-center py-8 text-lg">Loading...</div>}
-//       />
-//       <ToastContainer />
-//     </AuthProvider>
-//   </StrictMode>
-// );
+
 
 
 
