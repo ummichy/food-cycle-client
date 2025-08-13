b6f228265ccc94e4b876cc0f2de3dfb7ffa393cd
rowshonummi <rowshonummichowdhury@gmail.com>
2025-08-13 22:51:01 +0600

    done

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,20 @@
+// // src/pages/ErrorPage.jsx
+// import React from "react";
+// import { Link } from "react-router";
+
+// const ErrorPage = ({ code = "Error", message = "Something went wrong." }) => {
+//   return (
+//     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-50 via-gray-100 to-gray-200 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 text-gray-900 dark:text-gray-100 px-6">
+//       <h1 className="text-8xl font-extrabold mb-4 select-none">{code}</h1>
+//       <p className="text-lg text-center mb-6 max-w-lg">{message}</p>
+//       <Link
+//         to="/"
+//         className="px-6 py-3 bg-black text-white rounded-full hover:bg-gray-800 transition"
+//       >
+//         Back to Home
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default ErrorPage;
